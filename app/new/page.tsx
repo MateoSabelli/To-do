a202883a0c8e283b59be415dbcb0f176800a5203
Mateo Sabelli <mateosabelli4a@gmail.com>
2024-09-23 19:39:22 -0300

    dafsfa

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { TaskForm } from './Task-form'
 
 const NewPage = () => {
--- app/tasks/[id]/edit/page.tsx
+++ app/tasks/[id]/edit/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { TaskForm } from "@/app/new/Task-form";
 import prisma from "@/lib/prisma";
 import { redirect } from "next/navigation";
@@ -17,6 +18,7 @@ export default async function TaskPageEdit({params}:{params:{id:string}}){
 
     
     return(
+        // eslint-disable-next-line react/react-in-jsx-scope
         <div className='flex items-center justify-center h-screen'>
             <TaskForm task={task}/>
         </div>
